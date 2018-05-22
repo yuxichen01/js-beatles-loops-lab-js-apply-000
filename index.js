eae26e311ef007c0bd576bde80ff246d00729d59
yuxichen11@gmail.com <yuxichen11@gmail.com>
2018-05-22 17:32:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,9 +16,16 @@ function johnLennonFacts(facts) {
 }
 function iLoveTheBeatles(n) {
   var emptyN = [];
+<<<<<<< HEAD
   do {
     n++;
     emptyN.push("I love the Beatles!");
   } while (n < 15);
+=======
+  var i = 0;
+  do {
+    emptyN.push("I love the Beatles!");
+  } while (i < 15);
+>>>>>>> b1c7b0c458a1bbd42eb851dc95c6ae07ee6a859c
   return emptyN;
 }
